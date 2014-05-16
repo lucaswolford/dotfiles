8eddbf71023e2a6da3b8b264dadf0436cdfe0212
Lucas Wolford <lucas.wolford@gmail.com>
2014-05-16 16:48:47 -0400

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ln -s .bash_aliases ../.bash_aliases
+ln -s .bash_profile ../.bash_profile
+ln -s .bashrc ../.bashrc
+ln -s .git ../.git
+ln -s .gitignore ../.gitignore
+ln -s .tmux.conf ../.tmux.conf
+ln -s .vim ../.vim
+ln -s .vimrc ../.vimrc
