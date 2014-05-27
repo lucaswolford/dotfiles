43660f41c134d43a63d3eff56fe4dfd0d03e3636
Lucas Wolford <lucas@visionssky.com>
2014-05-27 10:51:50 -0400

    Remove ignored files.

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-ln -s .bash_aliases ../.bash_aliases
-ln -s .bash_profile ../.bash_profile
-ln -s .bashrc ../.bashrc
-ln -s .git ../.git
-ln -s .gitignore ../.gitignore
-ln -s .tmux.conf ../.tmux.conf
-ln -s .vim ../.vim
-ln -s .vimrc ../.vimrc
+ln -s dotfiles/.bash_aliases ../.bash_aliases
+ln -s dotfiles/.bash_profile ../.bash_profile
+ln -s dotfiles/.bashrc ../.bashrc
+ln -s dotfiles/.git ../.git
+ln -s dotfiles/.gitignore ../.gitignore
+ln -s dotfiles/.tmux.conf ../.tmux.conf
+ln -s dotfiles/.vim ../.vim
+ln -s dotfiles/.vimrc ../.vimrc
---
+++ uninstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm ../.bash_aliases
+rm ../.bash_profile
+rm ../.bashrc
+rm ../.git
+rm ../.gitignore
+rm ../.tmux.conf
+rm ../.vim
+rm ../.vimrc
+
